0c40378d63ba04105961fcdd2360b045ea5ae8c4
laurentziudmm <laurentziudmm@gmail.com>
2020-01-24 08:53:42 +0200

    AssertJ Second Test

--- src/test/java/AssertJTest.java
+++ src/test/java/AssertJTest.java
@@ -17,4 +17,18 @@ public class AssertJTest {
                 .contains("cde")
                 .doesNotContain("xyz");
     }
+
+    @Test
+    public void testAssertJ2() {
+        String text = "abcdef";
+
+        assertThat(text).isEqualTo("abcdef").contains("d");
+
+        String[] stringArray = {"abcde", "cdefg", "vbner","wedsfr","234566"};
+
+        assertThat(stringArray)
+                .hasSize(5)
+                .contains("234566")
+                .doesNotContain("wedsf3");
+    }
 }
